b45a2c734fc6c753ec196a7cf0e298f7dc0dd3b0
Jibbril Berbres <jibbril.berbres@viego.se>
2023-12-02 17:34:16 +0100

    Setup telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,14 +84,35 @@ require('lazy').setup({
         opts = {},
     },
 
-    -- Comment
+    -- Comment plugin
     {
         'numToStr/Comment.nvim',
         opts = {},
         lazy = false,
-    }
+    },
+    -- Fuzzy Finder (files, lsp, etc)
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+            -- Only load if `make` is available. Make sure you have the system
+            -- requirements installed.
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                -- NOTE: If you are having trouble with this installation,
+                --       refer to the README for telescope-fzf-native for more instructions.
+                build = 'make',
+                cond = function()
+                return vim.fn.executable 'make' == 1
+                end,
+            },
+        },
+    },
 })
 
+require('plugins.telescope')
 require('plugins.gitsigns')
 require('plugins.nvim-tree')
 require('plugins.onedark')
