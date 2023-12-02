e2fe0187c36c1ac1ac33d2da7dca99d46e4c4481
Jibbril Berbres <jibbril.berbres@viego.se>
2023-12-02 12:07:14 +0100

    Initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,25 @@
+-- lazy.vim configuration
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Lazy load plugins
+require('lazy').setup({
+    -- Git
+   'tpope/vim-fugitive'
+
+   -- Set tabstop and shiftwidth automatically
+   'tpope/vim-sleuth'
+})
+
+
