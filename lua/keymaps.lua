d2a48591df57363f98c8b80683a56d721ee96c11
Jibbril Berbres <jibbril.berbres@viego.se>
2023-12-03 01:01:53 +0100

    Move comment

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,7 @@
 -- --------------------------- general ---------------------------
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Remap leader
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
