bf36e937c9416119ab7a8d2df66973a2e78b32dd
Jibbril Berbres <jibbril.berbres@viego.se>
2023-12-17 18:02:14 +0100

    Remove sections from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -18,17 +18,17 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
+    lualine_a = {'filename'},
+    lualine_b = {'diagnostics'},
+    lualine_c = {},
+    lualine_x = {'filetype'},
+    lualine_y = {},
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_b = {'branch'},
+    lualine_c = {},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
