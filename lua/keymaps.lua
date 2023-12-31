382784f164c826bec8775d5493b19ee167e8a32c
Jibbril Berbres <jibbril.berbres@viego.se>
2023-12-31 03:23:25 +0100

    Setup basic luasnip config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -225,3 +225,32 @@ vim.keymap.set("n", "<C-y>", function() harpoon:list():select(4) end)
 --------------------------- nvim-ufo ---------------------------
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+
+--------------------------- luasnip ---------------------------
+local ls = require 'luasnip';
+
+vim.keymap.set("i", "<C-l>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+
+vim.keymap.set("i", "<C-h>", function()
+  if ls.jumpable() then
+    ls.jump(-1)
+  end
+end, { silent = true })
+
+vim.keymap.set("i", "<C-k>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, { silent = true })
+
+vim.keymap.set("i", "<C-j>", function()
+  if ls.choice_active() then
+    ls.change_choice(-1)
+  end
+end, { silent = true })
---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,20 @@
+-- Introductory video to snippet construction/functionality can be found below
+
+-- https://www.youtube.com/watch?v=FmHhonPjvvA
+
+local ls = require 'luasnip';
+local s = ls.snippet;
+local i = ls.insert_node;
+local fmt = require('luasnip.extras.fmt').fmt;
+local c = ls.choice;
+
+-- Rust println
+ls.add_snippets("rust", {
+  s("rpv", fmt(
+    [[
+      println!("{}{{:#?}}",{});
+    ]],
+    { i(1), i(2) }
+  ))
+})
+
