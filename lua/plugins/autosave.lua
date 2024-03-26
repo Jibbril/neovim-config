14bbe8bcef6dd09943f4400f82872e1b42249f53
Jibbril Berbres <jibbril.berbres@viego.se>
2024-03-26 22:41:40 +0100

    Solve issue with autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -11,16 +11,16 @@ require('auto-save').setup {
     -- function that determines whether to save the current buffer or not
     -- return true: if buffer is ok to be saved
     -- return false: if it's not ok to be saved
-    condition = function()
-        return true
-        -- local fn = vim.fn
-        -- local utils = require("auto-save.utils.data")
-        -- if
-        --     fn.getbufvar(buf, "&modifiable") == 1 and
-        --     utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-        --     return true        -- met condition(s), can save
-        -- end
-        -- return false           -- can't save
+    condition = function(buf)
+        -- return true
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if
+            fn.getbufvar(buf, "&modifiable") == 1 and
+            utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true        -- met condition(s), can save
+        end
+        return false           -- can't save
     end,
     write_all_buffers = false, -- write all buffers when the current one meets `condition`
     debounce_delay = 135,      -- saves the file at most every `debounce_delay` milliseconds
