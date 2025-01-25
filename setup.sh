3336ff48a4bf47ff410f0f12eed54febc20fcfee
Jibbril Berbres <jibbril.berbres@viego.se>
2025-01-25 22:26:17 +0100

    Modify install script to include readme steps

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 # Setup file which will copy perform actions to initialize the neovim config
 
+# Add repository, update and install dependencies
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
+sudo apt install make gcc ripgrep unzip git xclip neovim
+
+# Move files to .config
+mkdir -p ~/.config/nvim
+cp -r "$(dirname "$0")"/* ~/.config/nvim/
+
 # Set up debugpy for python debugger
-mkdir ~/.virtualenvs 
+mkdir -p ~/.virtualenvs 
 cd ~/.virtualenvs
 python3 -m venv debugpy
 debugpy/bin/python -m pip install debugpy
