b04bd2b49ecd3c18f7862bf45343227bd0f531ff
Jibbril Berbres <jibbril.berbres@viego.se>
2025-01-25 22:07:34 +0100

    Add initial setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Setup file which will copy perform actions to initialize the neovim config
+
+# Set up debugpy for python debugger
+mkdir ~/.virtualenvs 
+cd ~/.virtualenvs
+python3 -m venv debugpy
+debugpy/bin/python -m pip install debugpy
+
